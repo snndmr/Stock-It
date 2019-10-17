6254ef442ed01deda9bdf2a427e79e5b0e6f935c
snndmr <snndmr@users.noreply.github.com>
2019-10-18 02:32:44 +0300

    Dialog

---
+++ app/src/main/java/com/snn/stockapp/BottomDialog.java
@@ -0,0 +1,19 @@
+package com.snn.stockapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class BottomDialog extends BottomSheetDialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.room_dialog, container, false);
+    }
+}
