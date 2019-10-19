69bf0284c463aabef720d28c16d4864582c4db40
snndmr <snndmr@users.noreply.github.com>
2019-10-19 23:44:54 +0300

    Save System

--- app/src/main/java/com/snn/stockapp/MainActivity.java
+++ app/src/main/java/com/snn/stockapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.snn.stockapp;
 
+import android.content.SharedPreferences;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -18,30 +19,99 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private Toast toast;
     private ArrayList<Room> rooms;
+    private SharedPreferences sharedPreferences;
     private GridLayoutManager gridLayoutManager;
     private CustomAdapterRooms customAdapterRooms;
-    private Toast toast;
 
-    private ArrayList<Room> getRoomTestData() {
-        for (int i = 0; i < 50; i++) {
-            rooms.add(new Room(
-                    i % 5 == 0 ? "Bedroom" : i % 4 == 0 ? "Living Room" : i % 3 == 0 ? "Kitchen" : "Garage",
-                    i % 5 == 0 ? R.drawable.room_test_1 : i % 3 == 0 ? R.drawable.room_test_2 : i % 2 == 0 ? R.drawable.room_test_3 : R.drawable.room_test_4));
+    private void showToast(String text) {
+        if (this.toast != null) {
+            this.toast.cancel();
         }
-        return rooms;
+        this.toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+        this.toast.show();
+    }
+
+    private void saveData() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        String rooms_string = new Gson().toJson(rooms);
+
+        editor.putString("Rooms", rooms_string);
+        editor.apply();
+    }
+
+    private void loadData() {
+        String rooms_string = sharedPreferences.getString("Rooms", "");
+
+        if (!rooms_string.isEmpty()) {
+            Type type = new TypeToken<List<Room>>() {
+            }.getType();
+            List<Room> rooms = new Gson().fromJson(rooms_string, type);
+            this.rooms.addAll(rooms);
+        }
+    }
+
+    void showDialog(View view) {
+        Window window = MainActivity.this.getWindow();
+        window.getDecorView().getWindowVisibleDisplayFrame(new Rect());
+
+        ViewGroup viewGroup = findViewById(android.R.id.content);
+        View dialogView = LayoutInflater.from(view.getContext()).inflate(R.layout.room_dialog, viewGroup, false);
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this, R.style.CustomAlertDialog);
+        builder.setView(dialogView);
+        final AlertDialog alertDialog = builder.create();
+
+        final EditText editTextAdd = dialogView.findViewById(R.id.et_room_add);
+        final TextView textViewAdd = dialogView.findViewById(R.id.tv_room_add);
+        final ImageView imageViewCamera = dialogView.findViewById(R.id.iv_camera);
+
+        textViewAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editTextAdd.length() > 0) {
+                    alertDialog.dismiss();
+                    rooms.add(0, new Room(editTextAdd.getText().toString(), R.drawable.room_test_3));
+                    saveData();
+
+                    customAdapterRooms.notifyDataSetChanged();
+                    gridLayoutManager.scrollToPositionWithOffset(0, 0);
+
+                    getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+                    showToast("Oda eklendi");
+                } else {
+                    showToast("Odanın adını girmelisin");
+                }
+            }
+        });
+
+        imageViewCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Bu özellik yapım aşamasında");
+            }
+        });
+
+        alertDialog.show();
     }
 
     private void init() {
         TextView textView = findViewById(R.id.tv_app_name);
         textView.setBackground(GradientColors.TEXT_VIEW_BACKGROUND);
 
-        rooms = new ArrayList<>();
-        rooms = getRoomTestData();
+        this.rooms = new ArrayList<>();
+        this.sharedPreferences = getSharedPreferences("USER", MODE_PRIVATE);
+        loadData();
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -64,52 +134,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getWindow().getDecorView().setBackground(GradientColors.BACKGROUND);
         init();
-    }
-
-    private void showToast(String text) {
-        if (this.toast != null) {
-            this.toast.cancel();
-        }
-        this.toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
-        this.toast.show();
-    }
-
-    void showDialog(View view) {
-        Rect displayRectangle = new Rect();
-        Window window = MainActivity.this.getWindow();
-        window.getDecorView().getWindowVisibleDisplayFrame(displayRectangle);
-        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this, R.style.CustomAlertDialog);
-        ViewGroup viewGroup = findViewById(android.R.id.content);
-        View dialogView = LayoutInflater.from(view.getContext()).inflate(R.layout.room_dialog, viewGroup, false);
-        builder.setView(dialogView);
-        final AlertDialog alertDialog = builder.create();
-
-        final EditText editTextAdd = dialogView.findViewById(R.id.et_room_add);
-        final TextView textViewAdd = dialogView.findViewById(R.id.tv_room_add);
-        final ImageView imageViewCamera = dialogView.findViewById(R.id.iv_camera);
-
-        textViewAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (editTextAdd.length() > 0) {
-                    alertDialog.dismiss();
-                    rooms.add(0, new Room(editTextAdd.getText().toString(), R.drawable.room_test_3));
-                    customAdapterRooms.notifyDataSetChanged();
-                    gridLayoutManager.scrollToPositionWithOffset(0, 0);
-                    getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-                } else {
-                    showToast("Odanın adını girmelisin");
-                }
-            }
-        });
-
-        imageViewCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showToast("Bu özellik yapım aşamasında");
-            }
-        });
-
-        alertDialog.show();
     }
 }
